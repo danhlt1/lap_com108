259601ab2b58770c846ef6c7aa2427e69c13abc6
Danhlt <danhltpd08369@fpt.edu.vn>
2023-02-09 20:02:26 +0700

    first commit

---
+++ lab34/bai2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+float toan,ly, hoa;
+printf("nhap diem toan:");
+scanf("%f",&toan);
+printf("nhap diem hoa:");
+scanf("%f",&ly);
+printf("nhap diem ly:");
+scanf("%f",&hoa);
+
+}
