a7a094cbc1c4444b6b7dd481a9e1e7f16f014724
danhlt1 <124888057+danhlt1@users.noreply.github.com>
2023-02-28 08:42:56 +0700

    khoi tao

---
+++ bai1.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+	char str1[50];
+	printf ("nhap vao chuoi:\n");
+	gets(str1);
+	puts(str1);
+    int len = strlen(str1);
+    printf ("do dai chuoi co trong khoan la %d",len);
+    for (int i=0; i< len; i++){
+    	
+	}
+	if ()
+	}
---
+++ lap  1516.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+struct  sinhvien {
+	 	char tenSV [20];
+	 	char maSV [20];
+	 	int Malop [20];
+	 	int Namsinh [20];
+	 	char quequanSV [50];
+		char gioitinh [20];	
+};
+int main{
+
+}
